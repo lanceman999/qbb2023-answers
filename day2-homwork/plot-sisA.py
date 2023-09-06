d6f8ea81e0e26bb392c91c5f4297af28cf54388b
Lance <loconn13@jh.edu>
2023-09-06 16:01:26 -0400

    Feamle data plotting script

---
+++ day2-homwork/plot-sisA.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Get dataset to recreate Fig 3B from Lott et al 2011 PLoS Biology https://pubmed.gov/21346796
+# wget https://github.com/bxlab/cmdb-quantbio/raw/main/assignments/lab/bulk_RNA-seq/extra_data/all_annotated.csv
+
+transcripts = np.loadtxt( "/Users/cmdb/Downloads/Bootcamp_HW/Day2/all_annotated.csv", delimiter=",", usecols=0, dtype="<U30", skiprows=1 )
+print( "transcripts: ", transcripts[0:5] )
+
+samples = np.loadtxt( "/Users/cmdb/Downloads/Bootcamp_HW/Day2/all_annotated.csv", delimiter=",", max_rows=1, dtype="<U30" )[2:]
+print( "samples: ", samples[0:5] )
+
+data = np.loadtxt( "/Users/cmdb/Downloads/Bootcamp_HW/Day2/all_annotated.csv", delimiter=",", dtype=np.float32, skiprows=1, usecols=range(2, len(samples) + 2) )
+print( "data: ", data[0:5, 0:5] )
+
+# Find row with transcript of interest
+for i in range(len(transcripts)):
+    if transcripts[i] == 'FBtr0073461':
+        row = i
+
+# Find columns with samples of interest
+cols = []
+for i in range(len(samples)):
+    if "female" in samples[i]:
+        cols.append(i)
+
+# Subset data of interest
+expression = data[row, cols]
+
+# Prepare data
+#x = samples[cols]
+y = expression
+
+
+x = [10, 11, 12, 13, "14A","14B", "14C", "14D"]
+
+# Plot data
+fig, ax = plt.subplots()
+ax.set_title("sisA(FBtr0073461)")
+ax.plot(x,y)
+ax.set_xlabel("Developmental Stage")
+ax.set_ylabel("mRNA Expression")
+
+ax.legend()
+plt.xticks(rotation = 90)
+plt.tight_layout()
+plt.show()
+
+
+fig.savefig("FBtr0073461.png")
+plt.close(fig)

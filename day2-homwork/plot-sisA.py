362652c0d2f678618d0bc16d510e2e26462e526f
Lance <loconn13@jh.edu>
2023-09-06 17:15:24 -0400

    HW 1 Answers

--- day2-homwork/plot-sisA.py
+++ day2-homwork/plot-sisA.py
@@ -1,19 +1,39 @@
 #!/usr/bin/env python
 
-import numpy as np
+#import numpy as np
 import matplotlib.pyplot as plt
 
 # Get dataset to recreate Fig 3B from Lott et al 2011 PLoS Biology https://pubmed.gov/21346796
 # wget https://github.com/bxlab/cmdb-quantbio/raw/main/assignments/lab/bulk_RNA-seq/extra_data/all_annotated.csv
 
+"""
 transcripts = np.loadtxt( "/Users/cmdb/Downloads/Bootcamp_HW/Day2/all_annotated.csv", delimiter=",", usecols=0, dtype="<U30", skiprows=1 )
 print( "transcripts: ", transcripts[0:5] )
-
+"""
 samples = np.loadtxt( "/Users/cmdb/Downloads/Bootcamp_HW/Day2/all_annotated.csv", delimiter=",", max_rows=1, dtype="<U30" )[2:]
 print( "samples: ", samples[0:5] )
 
 data = np.loadtxt( "/Users/cmdb/Downloads/Bootcamp_HW/Day2/all_annotated.csv", delimiter=",", dtype=np.float32, skiprows=1, usecols=range(2, len(samples) + 2) )
 print( "data: ", data[0:5, 0:5] )
+
+
+f = open("/Users/cmdb/Downloads/Bootcamp_HW/Day2/all_annotated.csv", "r")
+lines = f.readlines()
+
+minus_header = lines[1:]
+
+transcript = []
+for line in minus_header:
+    line = line.rstrip()
+    line_list = line.split(",")
+    transcript.append(line_list[0])
+print(transcript[0:5])
+
+for i in lines:
+    transcript = line[i].split(",")
+    print(transript)
+
+
 
 # Find row with transcript of interest
 for i in range(len(transcripts)):
@@ -43,7 +63,7 @@ expression_m = data[row_m, cols_m]
 #x = samples[cols]
 y = expression
 y_m = expression_m
-y_m2 = 2*expression_m
+#y_m2 = np.array(expression_m)*2
 
 x = [10, 11, 12, 13, "14A","14B", "14C", "14D"]
 
@@ -52,7 +72,7 @@ fig, ax = plt.subplots()
 ax.set_title("sisA(FBtr0073461) Female & Male")
 ax.plot(x,y, c = "red", label = "Female")
 ax.plot(x,y_m, c = "blue", label = "Male")
-ax.plot(x,y_m2, c = "green", label = "2*Male")
+#ax.plot(x,y_m2, c = "green", label = "2*Male")
 ax.set_xlabel("Developmental Stage")
 ax.set_ylabel("mRNA Expression")
 ax.set_ylim(0,250)

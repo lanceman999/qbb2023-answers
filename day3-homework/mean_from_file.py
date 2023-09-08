4dd32a6b0ffd93cc007bf9583cef748e2dfd42ba
Lance <loconn13@jh.edu>
2023-09-08 09:15:21 -0400

    HW3 exercise 1 - updatecode!

--- day3-homework/mean_from_file.py
+++ day3-homework/mean_from_file.py
@@ -2,19 +2,16 @@
 
 import sys
 
-fname = sys.argv[1] #fname is my_integers.txt"
-data = open(fname).readlines()
-#print(data)
-
-def mean_int(a):
+def mean_int(filename):
+	fname = open(filename, 'r') #fname is my_integers.txt"
+	data = fname.readlines()
+	print(data)
 	int_list = []
 	for i in data:
 		i = i.rstrip("\n")
 		int_list.append(int(i))
 		total = sum(int_list)
-		mean = total/len(a)
+		mean = total/len(data)
 	return mean
 
-print(mean_int(data))
-
-# type "./mean_from_file.py my_integers.txt" into command line
+print(mean_int(sys.argv[1])) # type "./mean_from_file.py my_integers.txt" into command line

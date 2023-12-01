fd62f039f9232afe69352f770aea253d9c9505a4
Lance <loconn13@jh.edu>
2023-12-01 13:02:10 -0500

    final script

--- QBIO_class/Week10_Dec1/plotting_exercise2_HW10.py
+++ QBIO_class/Week10_Dec1/plotting_exercise2_HW10.py
@@ -30,37 +30,44 @@ fig.savefig("counts_of_sightings_UFO.png")
 
 ### PLOT 2 ###
 # Number of sightings over time - line
-"""
+
+time_of_sighting_df = UFO_data_df['day_part'].value_counts()
+
+print(time_of_sighting_df)
+
+order_list = ['night','astronomical dawn','nautical dawn','civil dawn','morning','afternoon','civil dusk','nautical dusk','astronomical dusk']
+time_of_sighting_df = time_of_sighting_df.loc[order_list]
+print(time_of_sighting_df)
+count = time_of_sighting_df.values
 
 fig,ax = plt.subplots()
 
-plt.plot()
-ax.set_xlabel("")
-ax.set_ylabel("")
-ax.set_title("")
+plt.plot(order_list, count, color = 'green')
+ax.set_xlabel("Time of Sighting")
+ax.set_ylabel("Number of Sightings")
+ax.set_title("Time of Day of Sightings")
 
 plt.tight_layout()
 plt.show()
 
-fig.savefig(".png")
-
-
+fig.savefig("day_time_sighting.png")
 
 
 ### PLOT 3 ###
 # During of sightings - histogram
 
-fig,ax = plt.subplots()
+final_data_df = UFO_data_df[UFO_data_df['duration_seconds'] < 4000]
 
-plt.hist()
-ax.set_xlabel("")
-ax.set_ylabel("")
-ax.set_title("")
+fig, ax = plt.subplots()
 
-plt.tight_layout()
+ax.hist(final_data_df['duration_seconds'], color = 'pink', bins = 50)
+ax.set_xlabel("Reported Duration (seconds)")
+ax.set_ylabel("Number of Sightings")
+ax.set_title("Distribution of Duration of UFO Sightings")
+fig.tight_layout()
 plt.show()
 
-fig.savefig(".png")
-"""
+fig.savefig("duration_of_sighting.png")
+
 
 
